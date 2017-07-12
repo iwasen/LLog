33bc504adca50b9fd60b02cfc74ae96ea0dbe8c4
aizawa <aizawa@troot.co.jp>
2017-07-12 15:51:37 +0900

    init

---
+++ app/src/main/java/jp/co/troot/llog/PhotoData.java
@@ -0,0 +1,11 @@
+package jp.co.troot.llog;
+
+import java.util.Date;
+
+public class PhotoData {
+	public int mSeqNo;
+	public Date mDate;
+	public String mFolderName;
+	public String mFileName;
+	public boolean mLocationFlag;
+}
