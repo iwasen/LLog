1f1a8cc78198cc37a516321c982753c17a46a6ff
Shinichi Aizawa <aizawa@troot.co.jp>
2021-03-13 19:06:48 +0900

    ホームボタンで終了

--- app/src/main/java/jp/co/troot/llog/MyActivity.kt
+++ app/src/main/java/jp/co/troot/llog/MyActivity.kt
@@ -3,9 +3,46 @@ package jp.co.troot.llog
 import android.app.Activity
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningAppProcessInfo
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
 import android.provider.Settings
 
-abstract class MyActivity : Activity() {
+
+open class MyActivity : Activity() {
+    private var homeButtonReceiver: HomeButtonReceive? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /** レシーバーを登録  */
+        homeButtonReceiver = HomeButtonReceive()
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+        registerReceiver(homeButtonReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        /** レシーバーの登録を解除  */
+        if (homeButtonReceiver != null) {
+            unregisterReceiver(homeButtonReceiver)
+            homeButtonReceiver = null
+        }
+    }
+
+    /** HOMEボタンの押下を受け取るレシーバー  */
+    class HomeButtonReceive : BroadcastReceiver() {
+        override fun onReceive(arg0: Context?, arg1: Intent?) {
+            if (arg0 is Activity) {
+                arg0.finish()
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         if (!mForeground) {
@@ -36,11 +73,6 @@ abstract class MyActivity : Activity() {
             }
         }
         return false
-    }
-
-    // ホームボタンが押されたら終了する（次回最初から起動させるため）
-    override fun onUserLeaveHint() {
-        finish()
     }
 
     companion object {
